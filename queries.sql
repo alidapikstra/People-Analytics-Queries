07a1f767c1179dda246023d731f3d564715119e9
Alida Pikstra <aampikstra@gmail.com>
2022-01-11 10:49:03 +0100

    Practice of comparison operators

--- queries.sql
+++ queries.sql
@@ -73,3 +73,62 @@ names of employees that do not contain “Jack”.
 */
 select * from employees where first_name not like ("%Jack%");
 
+/*
+Select all the information from the “salaries” table regarding contracts from 66,000 to 70,000 dollars per year.
+*/
+select * from salaries where salary between "66000" and "70000";
+
+/*
+Retrieve a list with all individuals whose employee number is not between ‘10004’ and ‘10012’.
+*/
+select * from employees where emp_no not between "10004" and "10012";
+
+/*
+Select the names of all departments with numbers between ‘d003’ and ‘d006’.
+*/
+select * from departments where dept_no between "d003" and "d006";
+
+/*
+Select the names of all departments whose department number value is not null.
+*/
+SELECT 
+    dept_name
+FROM
+    departments
+WHERE
+    dept_no IS NOT NULL;
+    
+/*
+Retrieve a list with data about all female employees who were hired in the year 2000 or after.
+*/
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    hire_date >= '2000-01-01'
+        AND gender = 'F';
+
+/*
+Extract a list with all employees’ salaries higher than $150,000 per annum.
+*/
+SELECT 
+    *
+FROM
+    salaries
+WHERE
+    salary > '150000';
+
+/*
+Obtain a list with all different “hire dates” from the “employees” table.
+*/
+SELECT DISTINCT
+    hire_date
+FROM
+    employees;
+    
+/*
+How many annual contracts with a value higher than or equal to $100,000 have been registered in the salaries table?
+*/
+select count(*) from salaries where salary >= "100000";
+
